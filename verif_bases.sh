03558a2e5d27f5eeb35fd9c3462baffd234e46c1
ask-fred <68000028+ask-fred@users.noreply.github.com>
2020-09-30 14:11:39 +0200

    Update verif_bases.sh

--- verif_bases.sh
+++ verif_bases.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-. /var/lib/pgsql/.bash_profile
+. $HOME/.bash_profile
 
 FICHIER_TMP=/tmp/synchro_des_bases
 
@@ -7,25 +7,21 @@ echo '' > $FICHIER_TMP
 
 # Construction du corps du message si besoin
 
-Reference=`$PGBIN/psql -U postgres --host $DELEGATE_IP --port=9999 --dbname postgres -c "/*NO LOAD BALANCE*/ SELECT txid_current_snapshot(); " |grep ':' `
-for Machine in `$PGBIN/psql -U postgres --host $DELEGATE_IP --port=9999 --dbname postgres -c "show pool_nodes"|grep up |awk '{print $3}' `
+Reference=`$PGBIN/psql -U $USER -h $DELEGATE_IP -p 9999 -d $BASE -c "/*NO LOAD BALANCE*/ SELECT txid_current_snapshot(); " |grep ':' `
+for Machine in `$PGBIN/psql -U $USER -h $DELEGATE_IP -p 9999 -d $BASE -c "show pool_nodes"|grep up |awk '{print $3}' `
 do
-Verif=`$PGBIN/psql -U postgres --host $Machine --port=5432 --dbname postgres -c "SELECT txid_current_snapshot(); "|grep ':' `
+Verif=`$PGBIN/psql -U $USER -h $Machine -p 5432 -d $BASE -c "SELECT txid_current_snapshot(); "|grep ':' `
 test $Machine != $Reference && MESSAGE=' synchro ok '$Reference' '$Machine || MESSAGE=' synchro nok '$Reference' '$Verif' '$Machine
 echo $MESSAGE >> $FICHIER_TMP
 done
 
-echo 'base primaire sur' > /var/lib/pgsql/primaire
-$PGBIN/psql -U postgres --host $DELEGATE_IP --port=9999 --dbname postgres -c "show pool_nodes" |grep primary |awk '{print $3}' >> /var/lib/pgsql/primaire
-echo 'pool primaire sur' >> /var/lib/pgsql/primaire
-pcp_watchdog_info -h 10.172.236.140 -p 9898 -U postgres -w |grep MASTER >> /var/lib/pgsql/primaire
+echo 'base primaire sur' > $HOME/primaire
+$PGBIN/psql -U $USER -h $DELEGATE_IP -p 9999 -d $BASE -c "show pool_nodes" |grep primary |awk '{print $3}' >> $HOME/primaire
 
-$PGBIN/psql -U postgres --host $DELEGATE_IP --port=9999 --dbname postgres -c "show pool_nodes"|grep down > /tmp/bases_down
-for Machine in `$PGBIN/psql -U postgres --host $DELEGATE_IP --port=9999 --dbname postgres -c "show pool_nodes"|grep standby|awk '{print $3}' `
+$PGBIN/psql -U $USER -h $DELEGATE_IP -p 9999 -d $BASE -c "show pool_nodes"|grep down > /tmp/bases_down
+for Machine in `$PGBIN/psql -U $USER -h $DELEGATE_IP -p 9999 -d $BASE -c "show pool_nodes"|grep standby|awk '{print $3}' `
 do
-scp /tmp/synchro_des_bases postgres@$Machine:/tmp/synchro_des_bases
-scp /tmp/bases_down  postgres@$Machine:/tmp/bases_down
-scp /var/lib/pgsql/primaire postgres@$Machine:/var/lib/pgsql/primaire
+scp /tmp/synchro_des_bases $USER@$Machine:/tmp/synchro_des_bases
+scp /tmp/bases_down  $USER@$Machine:/tmp/bases_down
+scp $HOME/primaire $USER@$Machine:$HOME/primaire
 done
-
-
